3b971fb7a747aa97991d2442fd584f54d44d3ddd
kdowd <kevin.dowd@gmail.com>
2024-10-24 15:29:38 +1300

    from class

--- filesUsers.cpp
+++ filesUsers.cpp
@@ -1,10 +1,48 @@
-
-
 #include <iostream>
 #include <filesystem>
+#include <fstream>
+#include <string>
+
+using namespace std;
+using namespace std::filesystem;
+
+vector<path> allUserPaths{};
+
 int main()
 {
-	std::cout << "Hello World2!\n";
+	filesystem::path directorypath = "employees";
+
+	if (exists(directorypath)) {
+
+		for (auto entry : directory_iterator(directorypath)) {
+			if (entry.is_regular_file()) {
+				//cout << entry.path().filename() << endl;
+				allUserPaths.push_back(entry.path());
+			}
+		}
+
+		for (auto i : allUserPaths) {
+			//cout << i << endl;
+
+		}
+
+		ifstream infile(allUserPaths.front());
+
+		if (infile.is_open()) {
+			string theline;
+
+			while (getline(infile, theline)) {
+				cout << theline << endl;
+			}
+
+			infile.close();
+		}
+
+	}
+
+
+
+
 
 
 }
